8c8a27870dd68cc03dc2ea9c46971f792ef790d0
Ishwar Jangid <ishwarjangid@Ishwars-MacBook-Air.local>
2018-07-22 22:14:03 +0530

    Redux setup

---
+++ posts/src/components/profiles.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+
+class Profiles extends Component {
+  render() {
+    return (
+      <div>
+      Profiles
+      </div>
+    )
+  }
+}
+export default Profiles;
