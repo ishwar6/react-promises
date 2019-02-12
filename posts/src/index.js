00311a77a3c29505b333306cf67772ab759b541e
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-12 21:24:03 +0530

    Update index.js

--- posts/src/index.js
+++ posts/src/index.js
@@ -6,3 +6,14 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
